1d375d6666bbaa900a0f98d11e01124670b8a722
“Better” <kang_hyeok@naver.com>
2022-04-26 23:12:06 +0900

    [SPRINT-1] 로그인 컴포넌트 구현 - antdesign 및 로그인 기본창 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Button } from '../stories/Button';
 import styles from '../styles/Home.module.css';
+import Login from '../components/Login';
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Button size="large" label="버튼" />
+        <Login />
       </main>
     </div>
   );
