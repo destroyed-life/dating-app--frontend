2dc6962ad6a7681ef87da344285f0b8b3c868ea8
or71enahr <kjjjs0825@gmail.com>
2022-04-29 22:26:35 +0900

    feat: [SPRINT-2] Create header and mainbanner for Login Page

---
+++ pages/login/index.tsx
@@ -0,0 +1,53 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
+
+const Login = () => {
+  return (
+    <header css={headerstyle} className="Header">
+      <h1 css={logostyle}>
+        <a href="#">
+          <img src="img/Log.svg" alt="logoimg"></img>
+        </a>
+      </h1>
+      <ul css={menustyle} className="menu">
+        <li css={liststyle}>
+          <a href="#">홈</a>
+        </li>
+        <li css={liststyle}>
+          <a href="#">타임라인</a>
+        </li>
+        <li css={liststyle}>
+          <a href="#">설정</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+const logostyle = css`
+  margin: 0;
+`;
+
+const headerstyle = css`
+  display: flex;
+  position: fixed;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  z-index: 100;
+`;
+
+const menustyle = css`
+  display: inline-block;
+  list-style: none;
+  height: 100%;
+  align-items: center;
+`;
+
+const liststyle = css`
+  display: inline-block;
+  align-items: center;
+  padding: 20px;
+`;
+
+export default Login;
